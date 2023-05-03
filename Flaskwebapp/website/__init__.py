6d61b1ab7bd6f99e37556f79c3d481d93a2d7d2e
rahul007 <rahuljai8267@gmail.com>
2023-05-04 02:26:12 +0530

    added the database models of User and Note and also updated the sign up function to receive the user data and add it to the database

--- Flaskwebapp/website/__init__.py
+++ Flaskwebapp/website/__init__.py
@@ -1,13 +1,24 @@
-from flask import Flask;
+from flask import Flask
+from flask_sqlalchemy  import SQLAlchemy
+from os import path 
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'MJSDHVCIHSDJNC DKJFCSLAK JSHFSA JAGDF'
+    app.config['SECRET_KEY'] = 'SDSAKJ AOIDJDAWDA'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- Flaskwebapp/website/auth.py
+++ Flaskwebapp/website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -14,14 +17,14 @@ def logout():
 def sign_up():
     if request.method =='POST':
         email = request.form.get('Email')
-        firstname = request.form.get('firstname')
+        firstName = request.form.get('firstname')
         lastName = request.form.get('lastname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 8:
             flash('Email must be greater than 7 character', category='error')
-        elif len(firstname) < 2:
+        elif len(firstName) < 2:
             flash('First Name must be at least 2 character long ', category='error')
         elif len(lastName) < 2:
             flash('last Name must be at least 2 character long ', category='error')
@@ -30,6 +33,10 @@ def sign_up():
         elif len(password1) < 8:
             flash('Length of the password must be at least 8 character long ', category='error')
         else:
+            new_user = User(Email=email, Firstname=firstName, Lastname=lastName, Password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('User added successfully ', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign-up.html')
