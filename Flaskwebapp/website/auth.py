24c671a2cdeba79c9b00676d335fae32fab35f46
rahul007 <rahuljai8267@gmail.com>
2023-05-06 21:29:26 +0530

    added if else condition in the nav bar for curent user logged in

--- Flaskwebapp/website/auth.py
+++ Flaskwebapp/website/auth.py
@@ -24,7 +24,7 @@ def login():
             flash('User does not exist', category='error')
 
 
-    return render_template('/login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -63,4 +63,4 @@ def sign_up():
             login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
-    return render_template('sign-up.html')
+    return render_template('sign-up.html', user=current_user)
