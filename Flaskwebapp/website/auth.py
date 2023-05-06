9c771dfa2b6c0c4f50fb44579397cda570c5b1e5
rahul007 <rahuljai8267@gmail.com>
2023-05-06 20:10:05 +0530

    added authentication to the logout page and

--- Flaskwebapp/website/auth.py
+++ Flaskwebapp/website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, url_for, redirect
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, logout_user, current_user, login_required
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.Password, password):
                 flash('logged in succesfully', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('incorrect password', category='error')
         else:
@@ -24,8 +27,10 @@ def login():
     return render_template('/login.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1>logout</h1>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
@@ -55,6 +60,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('User added successfully ', category='success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template('sign-up.html')
