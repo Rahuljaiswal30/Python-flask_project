38df4f55316ef9704f49abe75107287d8da1e282
rahul007 <rahuljai8267@gmail.com>
2023-05-10 00:08:05 +0530

    added Note authentication: Note will be added to the particular user only

--- Flaskwebapp/website/views.py
+++ Flaskwebapp/website/views.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from website.models import Note
+from . import db
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['POST', 'GET'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('note is too short.', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('note is added successfully.', category='success')
+
+
     return render_template('home.html', user=current_user)
